285f2af60ef0d9f03e592d209d4ac7adcd45a276
Anton Telyshev <anton.telishev@yandex.ru>
2024-05-10 11:03:30 +0300

    Support `uintptr` and `unsafe.Pointer` (#34)
    * Support uintptr and unsafe.Pointer
    
    * README: link to golangci-lint usage
    
    * More tests

--- pkg/analyzer/analyzer_test.go
+++ pkg/analyzer/analyzer_test.go
@@ -9,9 +9,22 @@ import (
 )
 
 func TestNilNil(t *testing.T) {
+	t.Parallel()
+
 	pkgs := []string{
 		"examples",
 		"strange",
+		"unsafe",
 	}
 	analysistest.Run(t, analysistest.TestData(), analyzer.New(), pkgs...)
+}
+
+func TestNilNil_Flags(t *testing.T) {
+	t.Parallel()
+
+	anlzr := analyzer.New()
+	if err := anlzr.Flags.Set("checked-types", "ptr"); err != nil {
+		t.Fatal(err)
+	}
+	analysistest.Run(t, analysistest.TestData(), anlzr, "pointers-only")
 }
--- pkg/analyzer/testdata/src/examples/negative.go
+++ pkg/analyzer/testdata/src/examples/negative.go
@@ -14,7 +14,23 @@ func withoutError2() (*User, *User)               { return nil, nil }
 func withoutError3() (*User, *User, *User)        { return nil, nil, nil }
 func withoutError4() (*User, *User, *User, *User) { return nil, nil, nil, nil }
 
+func invalidOrder() (error, *User)               { return nil, nil }
+func withError3rd() (*User, bool, error)         { return nil, false, nil }
+func withError4th() (*User, *User, *User, error) { return nil, nil, nil, nil }
+
+func slice() ([]int, error) { return nil, nil }
+
+func strNil() (string, error)   { return "nil", nil }
+func strEmpty() (string, error) { return "", nil }
+
 // Valid.
+
+func primitivePtrTypeValid() (*int, error) {
+	if false {
+		return nil, io.EOF
+	}
+	return new(int), nil
+}
 
 func structPtrTypeValid() (*User, error) {
 	if false {
@@ -23,11 +39,19 @@ func structPtrTypeValid() (*User, error) {
 	return new(User), nil
 }
 
-func primitivePtrTypeValid() (*int, error) {
+func unsafePtrValid() (unsafe.Pointer, error) {
 	if false {
 		return nil, io.EOF
 	}
-	return new(int), nil
+	var i int
+	return unsafe.Pointer(&i), nil
+}
+
+func uintPtrValid() (uintptr, error) {
+	if false {
+		return 0, io.EOF
+	}
+	return 0xc82000c290, nil
 }
 
 func channelTypeValid() (ChannelType, error) {
@@ -54,13 +78,6 @@ func ifaceTypeValid() (io.Reader, error) {
 }
 
 // Unsupported.
-
-func invalidOrder() (error, *User)               { return nil, nil }
-func withError3rd() (*User, bool, error)         { return nil, false, nil }
-func withError4th() (*User, *User, *User, error) { return nil, nil, nil, nil }
-func unsafePtr() (unsafe.Pointer, error)         { return nil, nil }
-func uintPtr() (uintptr, error)                  { return 0, nil }
-func slice() ([]int, error)                      { return nil, nil }
 
 func implicitNil1() (*User, error) {
 	err := (error)(nil)
