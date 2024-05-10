94056ec2e2b0ce7d42fc77b11026ea687571f9a0
Anton Telyshev <anton.telishev@yandex.ru>
2024-05-10 11:35:34 +0300

    checked-types flag: improve description

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -28,7 +28,7 @@ func New() *analysis.Analyzer {
 		Run:      n.run,
 		Requires: []*analysis.Analyzer{inspect.Analyzer},
 	}
-	a.Flags.Var(&n.checkedTypes, "checked-types", "comma separated list")
+	a.Flags.Var(&n.checkedTypes, "checked-types", "comma separated list of return types to check")
 
 	return a
 }
